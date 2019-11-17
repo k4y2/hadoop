2f86cba4332a2dbf198578c943f09b23a7f23d84
Tuan Anh Nguyen <kayz.info@gmail.com>
2019-11-17 15:39:02 +0700

    Add wordCount

---
+++ WordCount.java
@@ -0,0 +1,58 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class WordCount {
+    public static class E_Map extends MapReduceBase implements Mapper<
+            Text,
+            Text,
+            Text,
+            IntWritable> {
+
+        public void map(Text text, Text value, OutputCollector<Text, IntWritable> outputCollector, Reporter reporter) throws IOException {
+            String line = value.toString();
+            String[] words = line.split(" ");
+            for (String word : words
+                 ) {
+                outputCollector.collect(new Text(word), new IntWritable(1));
+            };
+        }
+    }
+
+    public static class E_Reduce extends MapReduceBase implements Reducer<
+            Text,
+            IntWritable,
+            Text,
+            IntWritable> {
+
+        public void reduce(Text text, Iterator<IntWritable> iterator, OutputCollector<Text, IntWritable> outputCollector, Reporter reporter) throws IOException {
+            int sum = 0;
+            while(iterator.hasNext()) {
+                sum += iterator.next().get();
+            }
+            outputCollector.collect(new Text(text), new IntWritable(sum));
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        JobConf jobConf = new JobConf();
+
+        jobConf.setJobName("Count Word");
+        jobConf.setMapperClass(E_Map.class);
+        jobConf.setCombinerClass(E_Reduce.class);
+        jobConf.setReducerClass(E_Reduce.class);
+        jobConf.setOutputKeyClass(Text.class);
+        jobConf.setOutputValueClass(IntWritable.class);
+        jobConf.setInputFormat(TextInputFormat.class);
+        jobConf.setOutputFormat(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(jobConf, new Path(args[0]));
+        FileOutputFormat.setOutputPath(jobConf, new Path(args[1]));
+
+        JobClient.runJob(jobConf);
+    }
+}
