15be1fc47ea1f5f6cd14752eb75eba3e36106145
Tuan Anh Nguyen <kayz.info@gmail.com>
2020-01-02 23:18:32 +0700

    Fix some little bug

--- java/Test.java
+++ java/Test.java
@@ -13,8 +13,18 @@ import java.net.URI;
 
 public class Test {
 
+    public static boolean compare(FSDataInputStream is1, FSDataInputStream is2, int k) throws IOException {
+        for (int j = 0; j < k; j++) {
+            String line1 = is1.readLine();
+            String line2 = is2.readLine();
+            if(!line1.equalsIgnoreCase(line2)) return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         int k = 2;
+        boolean stop = false;
         Configuration conf = new Configuration();
         conf.set("kmeans.k", k + "");
         InitMapper mapper = new InitMapper();
@@ -31,10 +41,10 @@ public class Test {
             job.setOutputValueClass(Text.class);
 
             int i = 0;
-            while(i < 2) {
-                if(i==1) {
+            while(i < 10 && !stop) {
+                if(i==0) {
                     Path input = new Path(args[0]);
-                    Path output = new Path(args[1]+"_m_"+i);
+                    Path output = new Path(args[1]+"/cal_"+i);
                     FileInputFormat.addInputPath(job, input);
                     FileOutputFormat.setOutputPath(job, output);
                     job.waitForCompletion(true);
@@ -42,18 +52,28 @@ public class Test {
                 else {
                     Configuration conf2 = new Configuration();
                     conf2.set("kmeans.k",k+"");
-                    String uri = args[1]+"_m_"+(i-1)+"/part-r-00000";
+                    String uri = args[1]+"/cal_"+(i-1)+"/part-r-00000";
                     Path path = new Path(uri);
                     Configuration conf3 = new Configuration();
                     FileSystem fs = FileSystem.get(URI.create(uri),conf3);
                     FSDataInputStream is = fs.open(path);
+                    if(i>1) {
+                        String uri2 = args[1]+"/cal_"+(i-2)+"/part-r-00000";
+                        Path path2 = new Path(uri2);
+                        Configuration conf4 = new Configuration();
+                        FileSystem fs2 = FileSystem.get(URI.create(uri2),conf4);
+                        FSDataInputStream is2 = fs2.open(path2);
+                        if(compare(is,is2,k)) {
+                            stop=true;
+                        }
+                    }
                     for (int j = 0; j < k; j++) {
                         String line = is.readLine();
                         String[] tmp = line.split("\\s+");
                         conf2.set("kmeans.center"+j, tmp[1]);
                     }
                     Job job2 = Job.getInstance(conf2);
-                    job2.setJobName("Calculating Cluster");
+                    job2.setJobName("Calculating Cluster "+i);
                     job2.setJarByClass(Test.class);
 
                     job2.setMapperClass(KmeansMapper.class);
@@ -63,7 +83,8 @@ public class Test {
                     job2.setOutputValueClass(Text.class);
 
                     FileInputFormat.addInputPath(job2, new Path(args[0]));
-                    FileOutputFormat.setOutputPath(job2, new Path(args[1]+"_m_"+i));
+                    FileOutputFormat.setOutputPath(job2, new Path(args[1]+"/cal_"+i));
+                    job2.waitForCompletion(true);
                 }
                 i++;
             }
