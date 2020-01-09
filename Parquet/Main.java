f2ceeed274dfc844dbdd132fe1a30e8250ca0439
Tuan Anh Nguyen <kayz.info@gmail.com>
2020-01-09 23:08:55 +0700

    Fix bug class

--- Parquet/Main.java
+++ Parquet/Main.java
@@ -1,7 +1,7 @@
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -9,12 +9,15 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 import org.apache.parquet.avro.AvroParquetOutputFormat;
 
+import java.io.File;
 import java.io.IOException;
 
 
-public class Main {
+public class Main extends Configured implements Tool {
     private static Schema schema;
 
     public class ConvertMapper extends Mapper<LongWritable,
@@ -22,43 +25,44 @@ public class Main {
             Void,
             GenericRecord> {
         private GenericRecord record = new GenericData.Record(schema);
+
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             String line = value.toString();
             String[] lineValues = line.split("\\s+");
-            record.put("id",lineValues[0]);
-            record.put("Name",lineValues[1]);
-            record.put("Dept",lineValues[2]);
+            record.put("id", lineValues[0]);
+            record.put("Name", lineValues[1]);
+            record.put("Dept", lineValues[2]);
             context.write(null, record);
         }
     }
+
+    public int run(String[] args) throws Exception {
+        Job job = Job.getInstance(getConf());
+
+        schema = new Schema.Parser().parse(new File(args[0]));
+        job.setJobName("Convert text to Parquet");
+        job.setMapperClass(ConvertMapper.class);
+        job.setNumReduceTasks(0);
+        job.setOutputKeyClass(Void.class);
+        AvroParquetOutputFormat.setSchema(job, schema);
+        job.setOutputValueClass(GenericRecord.class);
+        job.setOutputFormatClass(AvroParquetOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
     public static void main(String[] args) {
-        if(args.length != 3) {
+        if (args.length != 3) {
             System.err.println("<Avro File> <Input> <Output>");
             System.exit(-1);
         }
-        /* Some thing error here */
-        schema = new Schema.Parser().parse(args[0]);
-        Configuration conf = new Configuration();
         try {
-            Job job = Job.getInstance(conf);
-
-            job.setJobName("Convert text to Parquet");
-            job.setJarByClass(Main.class);
-            job.setMapperClass(ConvertMapper.class);
-            job.setNumReduceTasks(0);
-            job.setOutputKeyClass(Void.class);
-            job.setOutputValueClass(GenericRecord.class);
-            job.setOutputFormatClass(AvroParquetOutputFormat.class);
-
-            FileInputFormat.addInputPath(job,new Path(args[1]));
-            FileOutputFormat.setOutputPath(job,new Path(args[2]));
-            job.waitForCompletion(true);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+            int exitFlag = ToolRunner.run(new Main(), args);
+            System.exit(exitFlag);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
