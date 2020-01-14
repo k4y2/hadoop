19420e078787c138760f7a025ec9e8b9d4614bdd
Tuan Anh Nguyen <kayz.info@gmail.com>
2020-01-14 20:23:48 +0700

    Convert

--- ConvertParquet.java
+++ ConvertParquet.java
@@ -9,35 +9,32 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.parquet.avro.AvroParquetOutputFormat;
+import org.apache.parquet.example.data.Group;
 
 import java.io.File;
 import java.io.IOException;
 
 public class ConvertParquet{
 
-    private static Schema schema;
-
-//    /* Cach 1 */
-//    static {
-//        try {
-//            schema = new Schema.Parser().parse(new File("home/hadoop/emp.avsc"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
     public static class ConvertMapper extends Mapper<LongWritable, Text, Void, GenericRecord> {
+        GenericRecord record;
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             String line = value.toString();
             String[] values = line.split("\\s+");
-            Configuration conf = context.getConfiguration();
-            String schemaStr = conf.get("schema");
-            GenericRecord record = new GenericData.Record(new Schema.Parser().parse(schemaStr));
+
             record.put("id", Integer.parseInt(values[0]));
             record.put("Name", values[1]);
             record.put("Dept", values[2]);
             context.write(null, record);
+        }
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            Configuration conf = context.getConfiguration();
+            String schemaStr = conf.get("schema");
+            record = new GenericData.Record(new Schema.Parser().parse(schemaStr));
         }
     }
 
@@ -46,7 +43,7 @@ public class ConvertParquet{
         try {
             /* Cach 2 */
             File file = new File(args[0]);
-            schema = new Schema.Parser().parse(file);
+            Schema schema = new Schema.Parser().parse(file);
             conf.set("schema", String.valueOf(schema));
             Job job = Job.getInstance(conf, "Convert");
 
@@ -54,8 +51,10 @@ public class ConvertParquet{
             job.setJarByClass(ConvertParquet.class);
             job.setMapperClass(ConvertMapper.class);
             job.setOutputKeyClass(Void.class);
-            job.setOutputValueClass(GenericRecord.class);
+            job.setOutputValueClass(Group.class);
             job.setNumReduceTasks(0);
+            job.setOutputFormatClass(AvroParquetOutputFormat.class);
+            AvroParquetOutputFormat.setSchema(job,schema);
 
             FileInputFormat.addInputPath(job, new Path(args[1]));
             FileOutputFormat.setOutputPath(job, new Path(args[2]));
