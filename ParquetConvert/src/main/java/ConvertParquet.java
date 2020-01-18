6efd6287b5017f5f111b2502e95d5e64e5833f29
Tuan Anh Nguyen <kayz.info@gmail.com>
2020-01-18 14:56:27 +0700

    fix bug

--- ParquetConvert/src/main/java/ConvertParquet.java
+++ ParquetConvert/src/main/java/ConvertParquet.java
@@ -19,62 +19,92 @@ import java.io.IOException;
 
 public class ConvertParquet {
 
+    private static Schema schema;
+
+    /**
+     * Class Mapper để chuyển dữ liệu từ Text sang Parquet
+     * Mapper sẽ xử lý Text -> Avro
+     * OutputFormat sẽ xử lý đầu ra từ Avro -> Parquet
+     */
     public static class ConvertMapper extends Mapper<LongWritable, Text, Void, GenericRecord> {
+
+        private GenericRecord record;
+
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            // Đọc dữ liệu và đưa vào GenericRecord
             String line = value.toString();
             String[] values = line.split("\\s+");
-            Configuration conf = context.getConfiguration();
-            String schemaStr = conf.get("schema");
-            GenericRecord record = new GenericData.Record(new Schema.Parser().parse(schemaStr));
             record.put("id", Integer.parseInt(values[0]));
             record.put("Name", values[1]);
             record.put("Dept", values[2]);
             context.write(null,record);
         }
 
+        // Cài đặt Schema
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            Configuration conf = context.getConfiguration();
+            String schemaStr = conf.get("schema");
+            record = new GenericData.Record(new Schema.Parser().parse(schemaStr));
+        }
     }
 
-    public static class ReadMapper extends Mapper<LongWritable, Group, Void, Text> {
+    /**
+     * Class Mapper để đọc dữ liệu từ file Parquet
+     */
+    public static class ReadMapper extends Mapper<LongWritable, GenericRecord, Void, Text> {
         @Override
-        protected void map(LongWritable key, Group value, Context context) throws IOException, InterruptedException {
-            String s = value.toString();
+        protected void map(LongWritable key, GenericRecord value, Context context) throws IOException, InterruptedException {
+            String s = "ID:"+value.get("id")+" Name:"+value.get("Name")+" Dept:"+value.get("Dept");
             context.write(null, new Text(s));
         }
+    }
+
+    // Config convertJob
+    public static void doConvert(Configuration conf, String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        File file = new File(args[0]);
+        schema = new Schema.Parser().parse(file);
+        conf.set("schema", String.valueOf(schema));
+        Job job = Job.getInstance(conf, "Convert");
+
+        job.setJarByClass(ConvertParquet.class);
+        job.setMapperClass(ConvertMapper.class);
+        job.setOutputKeyClass(Void.class);
+        job.setOutputValueClass(Group.class);
+        job.setNumReduceTasks(0);
+        job.setOutputFormatClass(AvroParquetOutputFormat.class);
+        // Setup AvroParquet
+        AvroParquetOutputFormat.setSchema(job,schema);
+
+        FileInputFormat.addInputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.waitForCompletion(true);
+    }
+
+    // Config readJob
+    public static void doRead(Configuration conf, String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Job job2 = Job.getInstance(conf, "Read Parquet");
+        job2.setJarByClass(ConvertParquet.class);
+        job2.setMapperClass(ReadMapper.class);
+        job2.setOutputKeyClass(Void.class);
+        job2.setOutputValueClass(Text.class);
+        job2.setInputFormatClass(AvroParquetInputFormat.class);
+        // Setup AvroParquetInputFormat
+        AvroParquetInputFormat.setAvroReadSchema(job2, schema);
+        job2.setOutputFormatClass(TextOutputFormat.class);
+        FileInputFormat.addInputPath(job2, new Path(args[2]+"/part-m-00000.parquet"));
+        FileOutputFormat.setOutputPath(job2, new Path(args[2]+"/result"));
+        job2.setNumReduceTasks(0);
+        job2.waitForCompletion(true);
     }
 
     public static void main(String[] args) {
         Configuration conf = new Configuration();
         try {
-            File file = new File(args[0]);
-            Schema schema = new Schema.Parser().parse(file);
-            conf.set("schema", String.valueOf(schema));
-            Job job = Job.getInstance(conf, "Convert");
+            doConvert(conf, args);
+            doRead(conf, args);
 
-            job.setJarByClass(ConvertParquet.class);
-            job.setMapperClass(ConvertMapper.class);
-            job.setOutputKeyClass(Void.class);
-            job.setOutputValueClass(Group.class);
-            job.setNumReduceTasks(0);
-            job.setOutputFormatClass(AvroParquetOutputFormat.class);
-            AvroParquetOutputFormat.setSchema(job,schema);
-
-            FileInputFormat.addInputPath(job, new Path(args[1]));
-            FileOutputFormat.setOutputPath(job, new Path(args[2]));
-            job.waitForCompletion(true);
-
-            Job job2 = Job.getInstance(conf, "Read Parquet");
-            job2.setJarByClass(ConvertParquet.class);
-            job2.setMapperClass(ReadMapper.class);
-            job2.setOutputKeyClass(Void.class);
-            job2.setOutputValueClass(Text.class);
-            job2.setInputFormatClass(AvroParquetInputFormat.class);
-            AvroParquetInputFormat.setAvroReadSchema(job2, schema);
-            job2.setOutputFormatClass(TextOutputFormat.class);
-            FileInputFormat.addInputPath(job2, new Path(args[2]+"/part-m-00000.parquet"));
-            FileOutputFormat.setOutputPath(job2, new Path(args[2]+"/result"));
-            job2.setNumReduceTasks(0);
-            System.exit(job2.waitForCompletion(true) ? 0 : 1);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
